a93cb6f859eae9bbad2a1c7b3574327d86cd5cc0
cqroot <cqroot@outlook.com>
2022-10-09 18:24:30 +0800

    feat: only editors that include vi are supported

--- cmd/rename.go
+++ cmd/rename.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/jedib0t/go-pretty/v6/table"
@@ -80,7 +81,7 @@ func removeTmpFiles(oldFile string, newFile string) {
 
 func runEditor(oldFile string, newFile string) {
 	var editor string = os.Getenv("EDITOR")
-	if editor == "" {
+	if editor == "" || !strings.Contains(editor, "vi") {
 		editor = "vim"
 	}
 
